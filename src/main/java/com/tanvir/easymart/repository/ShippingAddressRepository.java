e02b8091e53488b81b9ef4777594314884f32553
tanvir70 <rifattanvir70@gmail.com>
2024-06-11 01:42:01 +0600

    Added new method

--- src/main/java/com/tanvir/easymart/repository/ShippingAddressRepository.java
+++ src/main/java/com/tanvir/easymart/repository/ShippingAddressRepository.java
@@ -2,6 +2,9 @@ package com.tanvir.easymart.repository;
 
 import com.tanvir.easymart.domain.ShippingAddress;
 
+import java.util.Optional;
+
 public interface ShippingAddressRepository {
     ShippingAddress save(ShippingAddress shippingAddress);
+    Optional<ShippingAddress> findOne(Long shippingAddressId);
 }
