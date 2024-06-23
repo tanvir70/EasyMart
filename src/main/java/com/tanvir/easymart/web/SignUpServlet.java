b3cf9b4b86e197fa9e576ed2a94e1c217ee582b5
tanvir70 <rifattanvir70@gmail.com>
2024-06-23 18:07:15 +0600

    Added CDI annotation

--- src/main/java/com/tanvir/easymart/web/SignUpServlet.java
+++ src/main/java/com/tanvir/easymart/web/SignUpServlet.java
@@ -2,55 +2,55 @@ package com.tanvir.easymart.web;// presentation layer
 
 import com.tanvir.easymart.dto.UserDTO;
 import com.tanvir.easymart.repository.JdbcUserRepositoryImpl;
-import com.tanvir.easymart.repository.UserRepository;
-import com.tanvir.easymart.repository.UserRepositoryImpl;
 import com.tanvir.easymart.service.UserService;
 import com.tanvir.easymart.service.UserServiceImpl;
 import com.tanvir.easymart.util.ValidationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
     private final static Logger LOGGER = LoggerFactory.getLogger(SignUpServlet.class);
-    private UserService userService = new UserServiceImpl(new JdbcUserRepositoryImpl());
+
+    @Inject
+    private UserService userService;
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LOGGER.info("serving signup page");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        LOGGER.info("serving singup page");
+
         req.getRequestDispatcher("/WEB-INF/signup.jsp").forward(req, resp);
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        UserDTO userDTO = copyParametersTo(req);
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        var userDTO = copyParametersTo(req);
         var errors = ValidationUtil.getInstance().validate(userDTO);
+
         if (!errors.isEmpty()) {
-            req.setAttribute("userDto",userDTO);
-            req.setAttribute("errors",errors);
+            req.setAttribute("userDto", userDTO);
+            req.setAttribute("errors", errors);
             LOGGER.info("User sent invalid data: {}", userDTO);
-            req.getRequestDispatcher("/WEB-INF/signup.jsp").forward(req,resp);
-        } else if(userService.inNotUniqueUserName(userDTO)) {
-            LOGGER.info("Username: {} is already exits.", userDTO.getUsername());
-            errors.put("username","The username already exits. Please use different username");
-            req.setAttribute("userDto",userDTO);
-            req.setAttribute("errors",errors);
-            req.getRequestDispatcher(
-                    "/WEB-INF/signup.jsp").forward(req, resp);
-        }else {
-            LOGGER.info("user is valid, creating a new user with: {}",userDTO);
+            req.getRequestDispatcher("/WEB-INF/signup.jsp").forward(req, resp);
+        } else if (userService.inNotUniqueUserName(userDTO)) {
+            LOGGER.info("Username: {} is already exist", userDTO.getUsername());
+            errors.put("username", "The username already exists. Please use a different username");
+            req.setAttribute("userDto", userDTO);
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher("/WEB-INF/signup.jsp").forward(req, resp);
+        } else {
+            LOGGER.info("user is valid, creating a new user with: {}", userDTO);
             userService.saveUser(userDTO);
             resp.sendRedirect("/login");
         }
@@ -61,8 +61,7 @@ public class SignUpServlet extends HttpServlet {
         userDTO.setFirstName(req.getParameter("firstName"));
         userDTO.setLastName(req.getParameter("lastName"));
         userDTO.setPassword(req.getParameter("password"));
-        userDTO.setPasswordConfirmed(req.getParameter(
-                "passwordConfirmed"));
+        userDTO.setPasswordConfirmed(req.getParameter("passwordConfirmed"));
         userDTO.setEmail(req.getParameter("email"));
         userDTO.setUsername(req.getParameter("username"));
 
